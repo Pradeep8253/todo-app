10fa7b2b5179e96b2bfb0a393a9f21dfc5be95a6
pradeep yadav <89512667+Pradeep8253@users.noreply.github.com>
2024-04-15 01:01:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,7 @@ import { Todos } from './components/Todos';
 function App() {
   const [todos, setTodos] = useState([]);
 
-  fetch("http://localhost:3000/todos").then(async function (req, res) {
+  fetch("http://localhost:3000/todos").then(async function ( res) {
     const json = await res.json();
     setTodos(json.todos);
   })
